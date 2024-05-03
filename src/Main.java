ed392cd97d8c437157176a0cd5f0a0eb2af3d6e2
haddoukcharfeddine <charfeddine.HADDOUK@esprit.tn>
2024-05-03 16:53:36 +0100

    creation de conflit par charfeddine

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("un conflit par charfeddine haddouk");
+        System.out.println("un conflit par charfeddine haddoukk");
 
         Scanner scanner = new Scanner(System.in);
 
