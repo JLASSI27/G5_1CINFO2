72265b6ab778d612c4c2d831984d1c33f3450cf9
haddoukcharfeddine <charfeddine.HADDOUK@esprit.tn>
2024-05-03 15:23:04 +0100

    upbdate

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,6 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("créer un conflit par charfeddine");
         Scanner scanner = new Scanner(System.in);
         // Choix de la fonction pour chaque membre
         System.out.println("Choisissez la fonction pour chaque membre:");
