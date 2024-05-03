f00eec2ae351b396769f113655ef8b23403493f8
haddoukcharfeddine <charfeddine.HADDOUK@esprit.tn>
2024-05-03 17:08:46 +0100

    Modification par charfeddine

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("un conflit par charfeddine");
+        System.out.println("un conflit par charfeddin");
 
         Scanner scanner = new Scanner(System.in);
 
